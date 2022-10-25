bc197777ceb5c9c0d13f4a7c23570a5c4da582ae
Michał Galiński <galinski.06@vp.pl>
2022-10-25 03:00:00 +0200

    Project cleanup

--- Source/FPSEscapeRoom/FPSEscapeRoomCharacter.cpp
+++ Source/FPSEscapeRoom/FPSEscapeRoomCharacter.cpp
@@ -17,19 +17,15 @@
 
 AFPSEscapeRoomCharacter::AFPSEscapeRoomCharacter()
 {
-	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
-	// set our turn rates for input
 	TurnRateGamepad = 27.5f;
 
-	// Create a CameraComponent	
 	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
 	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
-	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
 	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
 	Mesh1P->SetOnlyOwnerSee(true);
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
@@ -61,25 +57,8 @@ void AFPSEscapeRoomCharacter::SetForwardBreatheModeParameters(bool Forward)
 
 void AFPSEscapeRoomCharacter::BeginPlay()
 {
-	// Call the base class  
 	Super::BeginPlay();
 	SetForwardBreatheModeParameters(false);
-}
-
-// EDITOR ONLY
-void AFPSEscapeRoomCharacter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-
-	FName PropertyName = (PropertyChangedEvent.Property != NULL) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
-	if (PropertyName == GET_MEMBER_NAME_CHECKED(AFPSEscapeRoomCharacter, bForwardMode))
-	{
-		/* Because you are inside the class, you should see the value already changed */
-		if (GEngine)
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Zmiana!"));
-
-		SetForwardBreatheModeParameters(bForwardMode);
-	}
 }
 
 //////////////////////////////////////////////////////////////////////////// Input
@@ -102,10 +81,6 @@ void AFPSEscapeRoomCharacter::SetupPlayerInputComponent(class UInputComponent* P
 	// Bind movement events
 	PlayerInputComponent->BindAxis("Forward", this, &AFPSEscapeRoomCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("Strafe", this, &AFPSEscapeRoomCharacter::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "Mouse" versions handle devices that provide an absolute delta, such as a mouse.
-	// "Gamepad" versions are for devices that we choose to treat as a rate of change, such as an analog joystick
 	PlayerInputComponent->BindAxis("Turn", this, &AFPSEscapeRoomCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &AFPSEscapeRoomCharacter::LookUpAtRate);
 }
@@ -143,7 +118,6 @@ void AFPSEscapeRoomCharacter::EndTouch(const ETouchIndex::Type FingerIndex, cons
 
 void AFPSEscapeRoomCharacter::MoveForward(float Value)
 {
-	// add movement in that direction
 	AddMovementInput(GetActorForwardVector(), Value + bForwardMode * 2);
 }
 
@@ -151,20 +125,17 @@ void AFPSEscapeRoomCharacter::MoveRight(float Value)
 {
 	if (Value != 0.0f)
 	{
-		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
 	}
 }
 
 void AFPSEscapeRoomCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
 	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
 }
 
 void AFPSEscapeRoomCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
 	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
 }
 
@@ -183,9 +154,6 @@ void AFPSEscapeRoomCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	Bounce(bForwardMode);
-	///////////////////////////////////////////////////////////////////////////// FOR DEBUGGING ONLY
-	if (GetLocalViewingPlayerController()->WasInputKeyJustPressed(EKeys::Tab))
-		SetForwardBreatheModeParameters(!bForwardMode);
 }
 
 bool AFPSEscapeRoomCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent)
--- Source/FPSEscapeRoom/FPSEscapeRoomCharacter.h
+++ Source/FPSEscapeRoom/FPSEscapeRoomCharacter.h
@@ -39,9 +39,6 @@ public:
 protected:
 	virtual void BeginPlay();
 
-	// EDITOR ONLY
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
-
 public:
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Camera)
@@ -82,25 +79,14 @@ public:
 	FOnUseItem OnUseItem;
 protected:
 	
-	/** Fires a projectile. */
 	void OnPrimaryAction();
 
-	/** Handles moving forward/backward */
 	void MoveForward(float Val);
 
-	/** Handles strafing movement, left and right */
 	void MoveRight(float Val);
 
-	/**
-	 * Called via input to turn at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
 	void TurnAtRate(float Rate);
 
-	/**
-	 * Called via input to turn look up/down at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
 	void LookUpAtRate(float Rate);
 
 	void Bounce(bool bForwardModeOn);
@@ -121,22 +107,11 @@ protected:
 	TouchData	TouchItem;
 	
 protected:
-	// APawn interface
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
-	// End of APawn interface
-
-	/* 
-	 * Configures input for touchscreen devices if there is a valid touch interface for doing so 
-	 *
-	 * @param	InputComponent	The input component pointer to bind controls to
-	 * @returns true if touch controls were enabled.
-	 */
 	bool EnableTouchscreenMovement(UInputComponent* InputComponent);
 
 public:
-	/** Returns Mesh1P subobject **/
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
-	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
 };
